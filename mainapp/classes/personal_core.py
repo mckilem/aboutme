cbfd0360dc70ad248eec9a9c171d443970a2850b
Andrianov Georgiy <g.andrianov@htsts.ru>
2017-08-29 18:38:02 +0500

    adding hw_3

--- mainapp/classes/personal_core.py
+++ mainapp/classes/personal_core.py
@@ -1,47 +1,54 @@
 import datetime
-
+from mainapp.models import Skill
+from mainapp.models import Work
+from mainapp.models import Study
+from mainapp.models import Hobby
 
 class ClPersonalInfo:
     def __init__(self, name, date_of_birth):
         self.name = name
         self.date_of_birth = date_of_birth
         self.skills = []
+        Skill.objects.all().delete()
         self.hobbies = []
+        Hobby.objects.all().delete()
         self.studies = []
+        Study.objects.all().delete()
         self.works = []
+        Work.objects.all().delete()
 
     def __repr__(self):
         return str.format('{0}, Date of birth: {1}', self.name, self.date_of_birth)
 
-    def add_skill(self, skill):
-        if not skill in self.skills:
-            self.skills.append(skill)
-
-    def add_skill(self, skill):
-        if not skill in self.skills:
-            self.skills.append(skill)
-
-    def add_study(self, name, image, alt_text, link):
-        self.studies.append(ClStudy(name, image, alt_text, link))
-
-    def add_work(self, name, image, alt_text, link):
-        self.works.append(ClStudy(name, image, alt_text, link))
+    # def add_skill(self, skill):
+    #     if not skill in self.skills:
+    #         self.skills.append(skill)
+    #
+    # def add_skill(self, skill):
+    #     if not skill in self.skills:
+    #         self.skills.append(skill)
+    #
+    # #def add_study(self, name, image, alt_text, link):
+    #     self.studies.append(ClStudy(name, image, alt_text, link))
+    #
+    # def add_work(self, name, image, alt_text, link):
+    #     self.works.append(ClStudy(name, image, alt_text, link))
 
     def return_main_info_html(self):  # todo хорошо ли так писать...
         html = \
             '<ul>' \
-            '<li>' \
-            + self.name + \
-            '</li>' \
-            '<li>' \
-            + self.date_of_birth + \
-            '</li>' \
+                '<li>' \
+                    + self.name + \
+                '</li>' \
+                '<li>' \
+                    + self.date_of_birth + \
+                '</li>' \
             '</ul>'
 
     def init_defaults(self):
         self.name = 'Георгий'
         self.date_of_birth = datetime.date(1988, 6, 8)
-        self.skills = [
+        list_of_skills = [
             'Математика во всех ее проявлениях, все таки магистратуру мехмата осилил;',
             'Нейросети. Люблю нейросети.',
             'ООП, куда же без него',
@@ -56,7 +63,15 @@ class ClPersonalInfo:
             'Git, Jira, TFS, Redmine'
         ]
 
-        self.hobbies = [
+        self.skills = Skill.objects.all()
+        if len(self.skills) == 0:
+            for each_skill in list_of_skills:
+                skill = Skill()
+                skill.text = each_skill
+                skill.save()
+            self.skills = Skill.objects.all()
+
+        list_of_hobbies = [
             'Из айтишного: ИИ, big data, deep machine learning, распознавание текста и образов',
             'Моделизм (клеить и пытаться красить)',
             'Стрельба из лука и страйкбол (сейчас уже реже)',
@@ -64,35 +79,69 @@ class ClPersonalInfo:
             'Dungeons & Dragons в роли мастера и игрока'
         ]
 
-        self.add_study('Гимназия №17', 'images/study/school.jpg', 'школа', 'https://ru.wikipedia.org/wiki/'
-                                                                           'Гимназия_№_17_(Пермь)')
-        self.add_study('ПГНИУ', 'images/study/university.png', 'ПГНИУ', 'https://ru.wikipedia.org/wiki/Пермский_'
-                                                                        'государственный_университет')
-        self.add_study('GeekBrains.ru', 'images/study/gb.png', 'geekbrains logo', 'https://geekbrains.ru/')
+        self.hobbies = Hobby.objects.all()
+        if len(self.hobbies) == 0:
+            for each_hobby in list_of_hobbies:
+                hobby = Hobby()
+                hobby.text = each_hobby
+                hobby.save()
+            self.hobbies = Hobby.objects.all()
 
-        self.add_work('ПГНИУ', 'images/work/university.png', 'ПГНИУ', 'https://ru.wikipedia.org/wiki/Пермский_'
-                                                                      'государственный_университет')
-        self.add_work('ЗАО Прогноз', 'images/work/prognoz.jpeg', 'prognoz logo', 'http://www.prognoz.ru/')
+        self.studies = Study.objects.all()
+        if len(self.studies) == 0:
+            study = Study()
+            study.organization = 'Гимназия №17'
+            study.image_path = 'images/study/school.jpg'
+            study.site = 'https://ru.wikipedia.org/wiki/Гимназия_№_17_(Пермь)'
+            study.save()
 
+            study = Study()
+            study.organization = 'ПГНИУ'
+            study.image_path = 'images/study/university.png'
+            study.site = 'https://ru.wikipedia.org/wiki/Пермский_государственный_университет'
+            study.save()
 
-class ClStudy:
-    def __init__(self, name, image_path, alt_text, link):
-        self.name = name
-        self.image_path = image_path
-        self.alt_text = alt_text
-        self.link = link
+            study = Study()
+            study.organization = 'GeekBrains.ru'
+            study.image_path = 'images/study/gb.png'
+            study.site = 'https://geekbrains.ru/'
+            study.save()
+        self.studies = Study.objects.all()
 
-    def __repr__(self):
-        return str.format('{0}, {1}, {2}, {3}', self.name, self.image_path, self.alt_text, self.link)
+        self.works = Work.objects.all()
+        if len(self.works) == 0:
+            work = Work()
+            work.organization = 'ПГНИУ'
+            work.image_path = 'images/work/university.png'
+            work.site = 'https://ru.wikipedia.org/wiki/Пермский_государственный_университет'
+            work.save()
 
+            work = Work()
+            work.organization = 'ЗАО Прогноз'
+            work.image_path = 'images/work/prognoz.jpeg'
+            work.site = 'http://www.prognoz.ru/'
+            work.save()
+        self.works = Work.objects.all()
 
-class ClWork:
-    def __init__(self, name, image_path, alt_text, link):
-        self.name = name
-        self.image_path = image_path
-        self.alt_text = alt_text
-        self.link = link
-
-    def __repr__(self):
-        return str.format('{0}, {1}, {2}, {3}', self.name, self.image_path, self.alt_text, self.link)
+#
+# class ClStudy:
+#     def __init__(self, name, image_path, alt_text, link):
+#         self.name = name
+#         self.image_path = image_path
+#         self.alt_text = alt_text
+#         self.link = link
+#
+#     def __repr__(self):
+#         return str.format('{0}, {1}, {2}, {3}', self.name, self.image_path, self.alt_text, self.link)
+#
+#
+# class ClWork:
+#     def __init__(self, name, image_path, alt_text, link):
+#         self.name = name
+#         self.image_path = image_path
+#         self.alt_text = alt_text
+#         self.link = link
+#
+#     def __repr__(self):
+#         return str.format('{0}, {1}, {2}, {3}', self.name, self.image_path, self.alt_text, self.link)
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
-from django.db import models
 
 class Work(models.Model):
     organization = models.CharField(verbose_name='Organization', max_length=32)
-    site = models.CharField(verbose_name='Web page', max_length=64, blank=True)
+    site = models.CharField(verbose_name='Web page', max_length=255, blank=True)
     image_path = models.CharField(verbose_name='Image', max_length=255)
 
 class Study(models.Model):
     organization = models.CharField(verbose_name='Organization', max_length=32)
-    site = models.CharField(verbose_name='Web page', max_length=64, blank=True)
+    site = models.CharField(verbose_name='Web page', max_length=255, blank=True)
     image_path = models.CharField(verbose_name='Image', max_length=255)
-
 
 class Hobby(models.Model):
     text = models.TextField(verbose_name='Hobby description')
